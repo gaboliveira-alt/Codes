8668768087d3a778456498b841f5d581bfd0c563
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2024-10-24 17:11:58 -0300

    Add files via upload

---
+++ Python/problem1.py
@@ -0,0 +1,12 @@
+while True:
+    numbers = input().split()
+    n1 = int(numbers[0])
+    n2 = int(numbers[1])
+    
+    if (n1 == 0) and (n2 == 0):
+        break
+    
+    sum = str(n1 + n2).replace("0", "")
+    print(sum)
+    
+    
