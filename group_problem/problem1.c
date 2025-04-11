eeb3b323ddb268ee0ca7ee75f1c05bfd934b6512
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2025-04-11 01:28:49 -0300

    new lines

---
+++ group_problem/problem1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MAX_ELEMENTS 50
+#define MAX_GROUP 5
+
+
+typedef struct 
+{
+    int group_elements[MAX_ELEMENTS];
+    int size;
+}
+group;
+
+
+int main()
+{
+    group *new_groups[MAX_GROUP];
+}
